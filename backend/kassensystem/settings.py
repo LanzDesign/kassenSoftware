24b1b8f458a6514fd124630a3e2db34a646ec6bb
LanzDesign <128187584+LanzDesign@users.noreply.github.com>
2025-12-28 12:03:10 +0100

    .

--- backend/kassensystem/settings.py
+++ backend/kassensystem/settings.py
@@ -177,3 +177,6 @@ REST_FRAMEWORK = {
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Default primary key field type
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
