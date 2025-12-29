f52c0741f4bf91da57b6b13a4570c2474de4d70d
LanzDesign <128187584+LanzDesign@users.noreply.github.com>
2025-12-29 13:17:25 +0100

    Fix: Statisches Backend temporär für Django Admin

--- backend/kassensystem/settings.py
+++ backend/kassensystem/settings.py
@@ -196,7 +196,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
 
